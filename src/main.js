ef0d5603da78596ed9102ecce3c4b8708669e36d
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-02-08 22:04:16 -0300

    Finalizando exercício 1,1

--- src/main.js
+++ src/main.js
@@ -1,2 +1,19 @@
 import validator from 'validator';
-import './src/style.css';
+import './style.css';
+
+const campoDeTexto = document.querySelector('#value');
+const button = document.querySelector('#button');
+const select = document.querySelector('#option');
+const textoDeSaida = document.querySelector('#answer');
+
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  
+  const campos = {
+    cpf: validator.isTaxID(campoDeTexto.value, 'pt-BR'),
+    email: validator.isEmail(campoDeTexto.value),
+    url: validator.isURL(campoDeTexto.value),
+  };
+
+  textoDeSaida.innerHTML = `A validação retornou ${campos[select.value]}`;
+});
